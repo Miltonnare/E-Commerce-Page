974a32122848646f6f353fdfe0909a64daacee27
Miltonnare <miltonnareblessed@gmail.com>
2025-06-23 00:09:07 +0300

    Overlaying the Full Image

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
 
 function Images() {
+
+  const [overlayImage,setOverlayImage]=useState(null)
+
+  const thumbnails=[
+    {
+      thumb:"/image-product-1-thumbnail.jpg",
+      full:"/image-product-1.jpg",
+      alt:"Thembnail-1"
+    },
+    {
+      thumb:"/image-product-2-thumbnail.jpg",
+      full:"/image-product-2.jpg",
+      alt:"Thumbnail-2"
+    },
+    {
+      thumb:"/image-product-3-thumbnail.jpg",
+      full:"/image-product-3.jpg",
+      alt:"Thumbnail-3"
+    },
+    {
+      thumb:"/image-product-4-thumbnail.jpg",
+      full:"/image-product-4.jpg",
+      alt:"Thumbnail-4"
+    }
+
+  ]
   return (
     <div className="p-6 bg-white max-w-xl mx-auto">
       <img
@@ -8,39 +35,34 @@ function Images() {
         className="rounded-lg mb-4"
       />
       <div className="flex space-x-4">
-        <img
-          src="/image-product-1-thumbnail.jpg"
-          alt="Thumbnail 1"
+       {thumbnails.map( 
+        (img,index)=>(
+          <img 
+          key={index}
+          src={img.thumb}
+          alt={img.alt}
           className="w-16 h-16 rounded-lg cursor-pointer hover:ring-2 ring-orange-500"
           onClick={
-            ()=><img src="/image-product-1.jpg"/>
+            ()=>setOverlayImage(img.full)
           }
-        />
-        <img
-          src="/image-product-2-thumbnail.jpg"
-          alt="Thumbnail 2"
-          className="w-16 h-16 rounded-lg cursor-pointer hover:ring-2 ring-orange-500"
-          onClick={
-            ()=><img src="/image-product-2.jpg"/>
-          }
-        />
-        <img
-          src="/image-product-3-thumbnail.jpg"
-          alt="Thumbnail 3"
-          className="w-16 h-16 rounded-lg cursor-pointer hover:ring-2 ring-orange-500"
-          onClick={
-            ()=><img src="/image-product-3.jpg"/>
-          }
-        />
-        <img
-          src="/image-product-4-thumbnail.jpg"
-          alt="Thumbnail 4"
-          className="w-16 h-16 rounded-lg cursor-pointer hover:ring-2 ring-orange-500"
-          onClick={
-            ()=><img src="/image-product-4.jpg"/>
-          }
-        />
+          />
+        )
+       )}
+      
       </div>
+     {overlayImage && (
+  <div
+    className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50"
+    onClick={() => setOverlayImage(null)}
+  >
+    <img
+      src={overlayImage}
+      alt="Enlarged view"
+      className="max-w-[90%] max-h-[90%] rounded-lg shadow-2xl"
+    />
+  </div>
+)}
+
     </div>
   );
 }
