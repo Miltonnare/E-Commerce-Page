3f726d87537b886baab737bf388bf50acefdb9fd
Miltonnare <miltonnareblessed@gmail.com>
2025-06-23 11:03:19 +0300

    Feature: Add Minus Functionality

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,19 +2,43 @@ import { useState } from "react";
 import Minus from '../assets/icon-minus.svg';
 import Plus from '../assets/icon-plus.svg';
 import Cart from '../assets/icon-cart.svg';
+import Images from "./images";
 
 
 let price='$125.00';
 let percentDiscount='50%';
-let initialamount='$250.00';
+let initialamount='$250.00'
 
 function Details() {
+
+  const [quantity,setQuantity]=useState(0);
+
+function MinusFunction(){
+  setQuantity(
+    function (min){
+      if(min>0){
+        return min-1
+      }
+    
+    else 
+    return 0;
+    } 
+  )
+}
+
+function PlusFunction(){
+  setQuantity(
+    function(min){
+    return min+1
+    }
+  )
+}
   return (
     <div className="p-6 bg-white max-w-xl mx-auto">
-      <h4 className="text-orange-500 font-semibold uppercase tracking-widest text-sm mb-2">
+      <h4 className="text-gray-500 font-semibold uppercase tracking-widest text-sm mb-2">
         Sneaker Company
       </h4>
-      <h2 className="text-3xl font-bold mb-4">Fall Limited Edition Sneakers</h2>
+      <h2 className="text-5xl font-bold mb-4">Fall Limited Edition Sneakers</h2>
       <p className="text-gray-500 mb-4">
         These low profile sneakers are your perfect casual wear companion. Featuring a durable rubber 
         outer sole, they'll withstand everything the weather can offer.
@@ -31,9 +55,9 @@ function Details() {
 
       <div className="flex items-center space-x-4">
         <div className="flex items-center bg-gray-100 px-4 py-2 rounded gap-x-4">
-          <img src={Minus} alt="Minus icon" className="cursor-pointer" />
-          <span className="mx-3">0</span>
-          <img src={Plus} alt="Plus icon" className="cursor-pointer" />
+          <img src={Minus} alt="Minus icon" className="cursor-pointer" onClick={MinusFunction} />
+          <span className="mx-3">{quantity}</span>
+          <img src={Plus} alt="Plus icon" className="cursor-pointer" onClick={PlusFunction}/>
         </div>
         <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-6 rounded flex items-center space-x-2">
           <img src={Cart} alt="Cart icon" />
